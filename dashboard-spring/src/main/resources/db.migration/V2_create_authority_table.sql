ee2046570187412bd106573552c8904733e0258f
Remi <remi.turini@epitech.eu>
2021-05-18 01:18:31 +0200

    commit

--- dashboard-spring/src/main/resources/db.migration/V2_create_authority_table.sql
+++
@@ -1,4 +0,0 @@
-create table authority (
-    id int not null auto_increment primary key,
-    authority varchar(10) not null
-);
--- dashboard-spring/src/main/resources/db.migration/V3_create_user_authority_table.sql
+++
@@ -1,7 +0,0 @@
-create table user_authority (
-    id int not null auto_increment primary key,
-    user_id int not null,
-    authority_id int not null,
-    foreign key (user_id) references user(id),
-    foreign key (authority_id) references authority(id)
-);
